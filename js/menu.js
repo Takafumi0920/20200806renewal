55f55602c1396c938bec9af690f08ce1a78fec72
TakafumiYamamoto <>
2020-08-27 05:44:11 +0900

    メニューボタン実装完了

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,38 @@
 // ↓ハンバーガーメニューバツ切り替え
 $(function(){
 	
-	$('#menu-btn')
-			.on('click',function(){
-			$(this).find('#panel-btn-icon').toggleClass('close');
+	var duration = 400;
+	var $js__open = $('#js__open');
+	
+	var $menubtn = $('#menu-btn')
+	.on('click',function(){
+		$(this).find('#panel-btn-icon').toggleClass('switch');
 	})
+	.on('click',function(){
+		$js__open.stop(true).toggleClass('nav-open');
+
+//↓menu,close表示切り替え
+	if($menubtn.find('#panel-btn-icon').hasClass('switch')){
+		$menubtn.find('p > img')
+		.attr('src','images/close.svg');
+	}else{
+		$menubtn.find('p > img')
+		.attr('src', 'images/menu.svg');
+	};
+//↑↑
+	
+	if($js__open.hasClass('nav-open')){
+		$js__open.stop(true).animate({
+			position: 'fixed',
+			background: 'gray',
+			display: 'block'
+		},duration);
+	}else{
+		$js__open.stop(true).animate({
+			display: 'none'
+		},duration);
+	};
+});
 });
 //↑↑
 
@@ -16,7 +44,7 @@ $(function(){
 $(function(){
 	var duration = 300;
 	
-	$('#readmore > a')
+	$('.main-content_wrapper').find('.readmore > a')
 	.on('mouseover',function(){
 		$(this).stop(true).animate({
 			backgroundColor: '#1E2633',
